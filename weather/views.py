4a07bf1b5e20fd066a9dd7e3779be7e0f000f53d
mcstao <arislg99@gmail.com>
2024-07-10 22:52:58 +0600

    Ended weather view function

--- weather/views.py
+++ weather/views.py
@@ -5,7 +5,16 @@ from django.shortcuts import render
 def get_weather(request):
     weather_api_key = '100fb404761b0f4cab25d6bef491c7d6'
     city = 'Paris'
-    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={weather_api_key}'
-    result = requests.get(url)
-    print(result.text)
-    return render(request, 'get_weather.html')
+    lang = 'ru'
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={weather_api_key}&lang={lang}&units=metric'
+    data = requests.get(url).json()
+
+    weather_data = {
+        'city': data['name'],
+        'temperature': data['main']['temp'],
+        'humidity': data['main']['humidity'],
+        'wind_speed': data['wind']['speed'],
+        'wind_direction': data['wind']['deg']
+    }
+
+    return render(request, 'get_weather.html', {"weather": weather_data})
