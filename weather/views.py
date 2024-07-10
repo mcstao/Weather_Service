0121f79d7c5b82b32177efdef84f256d9ac5fa5a
mcstao <arislg99@gmail.com>
2024-07-10 22:31:41 +0600

    Created test view with weather template

--- weather/views.py
+++ weather/views.py
@@ -1,3 +1,11 @@
+import requests
 from django.shortcuts import render
 
-# Create your views here.
+
+def get_weather(request):
+    weather_api_key = '100fb404761b0f4cab25d6bef491c7d6'
+    city = 'Paris'
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={weather_api_key}'
+    result = requests.get(url)
+    print(result.text)
+    return render(request, 'get_weather.html')
